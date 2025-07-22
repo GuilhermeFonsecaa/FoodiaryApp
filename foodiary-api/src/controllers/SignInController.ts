86c3c99b421db6fca8241a586297c2a15f71c06f
Guilherme Fonseca <guilhermee.fonseca@outlook.com>
2025-07-22 00:54:00 -0300

    feat: add user authentication with email and password validation

--- foodiary-api/src/controllers/SignInController.ts
+++ foodiary-api/src/controllers/SignInController.ts
@@ -1,6 +1,10 @@
+import { eq } from "drizzle-orm";
+import { db } from "../db";
 import { signInSchema } from "../schemas/signInSchema";
 import { HttpRequest, HttpResponse } from "../types/Http";
-import { badRequest, ok } from "../utils/https";
+import { badRequest, ok, unauthorized } from "../utils/https";
+import { usersTable } from "../db/schema";
+import { compare } from "bcryptjs";
 
 export class SignInController {
   static async handle({ body }: HttpRequest): Promise<HttpResponse> {
@@ -10,8 +14,27 @@ export class SignInController {
       return badRequest({ errors: error.issues });
     }
 
+    const user = await db.query.usersTable.findFirst({
+      columns: {
+        id: true,
+        email: true,
+        password: true,
+      },
+      where: eq(usersTable.email, data.email),
+    });
+
+    if (!user) {
+      return unauthorized({ error: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await compare(data.password, user.password);
+
+    if (!isPasswordValid) {
+      return unauthorized({ error: "Invalid credentials" });
+    }
+
     return ok({
-      data,
+      user,
     });
   }
 }
