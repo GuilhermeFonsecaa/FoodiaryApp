5108ed0c2ec427214290df005873c25a1a39a1d7
Guilherme Fonseca <guilhermee.fonseca@outlook.com>
2025-07-22 00:53:22 -0300

    feat: validate existing email and hash password before creating user

--- foodiary-api/src/controllers/SignUpController.ts
+++ foodiary-api/src/controllers/SignUpController.ts
@@ -1,6 +1,10 @@
+import { eq } from "drizzle-orm";
+import { db } from "../db";
+import { usersTable } from "../db/schema";
 import { signUpSchema } from "../schemas/signUpSchema";
 import { HttpRequest, HttpResponse } from "../types/Http";
-import { badRequest, created } from "../utils/https";
+import { badRequest, conflict, created } from "../utils/https";
+import { hash } from "bcryptjs";
 
 export class SignUpController {
   static async handle({ body }: HttpRequest): Promise<HttpResponse> {
@@ -10,8 +14,38 @@ export class SignUpController {
       return badRequest({ errors: error.issues });
     }
 
+    const userAlreadyExists = await db.query.usersTable.findFirst({
+      columns: {
+        email: true,
+      },
+      where: eq(usersTable.email, data.account.email),
+    });
+
+    if (userAlreadyExists) {
+      return conflict({ error: "This email is already in use." });
+    }
+
+    const { account, ...rest } = data;
+
+    const hashedPassword = await hash(account.password, 10);
+
+    const [user] = await db
+      .insert(usersTable)
+      .values({
+        ...account,
+        ...rest,
+        password: hashedPassword,
+        calories: 0,
+        carbohydrates: 0,
+        fats: 0,
+        proteins: 0,
+      })
+      .returning({
+        id: usersTable.id,
+      });
+
     return created({
-      data,
+      userId: user.id,
     });
   }
 }
