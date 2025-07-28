3b09e71e45708db75b75d67877b2736fa044cc76
Guilherme Fonseca de Brito <guilherme.brito@groupsoftware.com.br>
2025-07-28 12:49:31 -0300

    chore: add custom fonts

--- foodiary-mobile/src/App.tsx
+++ foodiary-mobile/src/App.tsx
@@ -1,13 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import "./styles/global.css"
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from 'expo-splash-screen';
+import "./styles/global.css";
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+  useFonts,
+} from "@expo-google-fonts/host-grotesk";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View className='bg-lime-500 flex-1 items-center justify-center'>
-      <Text className='text-base'>Open App.tsx to start working on your app!</Text>
+    <View className="bg-lime-500 flex-1 items-center justify-center">
+      <Text className="font-sans-semibold">
+        Open App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
