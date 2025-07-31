3c321118d959228a6711e74a91111952f84d7e9f
Guilherme Fonseca <guilhermao1234@hotmail.com>
2025-07-31 02:23:41 -0300

    feat: add separator component

--- foodiary-mobile/src/components/MealCard.tsx
+++ foodiary-mobile/src/components/MealCard.tsx
@@ -1,20 +1,25 @@
 import { Text, TouchableOpacity, View } from "react-native";
 
-export function MealCard() {
+interface MealCardProps {
+  id: string;
+  name: string;
+}
+
+export function MealCard({ id, name }: MealCardProps) {
   return (
     <TouchableOpacity>
       <Text className="text-gray-700 font-sans-regular text-base">
         Hoje, 12h25
       </Text>
 
-      <View className="flex-row px-4 py-5 flex-row mt-2 gap-3 border border-gray-400 bg-white rounded-2xl">
+      <View className="flex-row px-4 py-5 mt-2 gap-3 border border-gray-400 bg-white rounded-2xl">
         <View className="size-12 bg-gray-200 rounded-full items-center justify-center">
           <Text>a</Text>
         </View>
 
         <View>
           <Text className="text-base text-gray-700 font-sans-regular">
-            Café da Manhã
+            {name}
           </Text>
           <Text className="font-sans-medium text-black-700 text-base">
             Pão, manteiga e café
