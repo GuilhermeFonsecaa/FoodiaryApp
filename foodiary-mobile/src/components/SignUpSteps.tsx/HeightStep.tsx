ea6cddaefba473537fa9f23ed841410a93105b9c
Guilherme Fonseca <guilhermao1234@hotmail.com>
2025-08-10 05:02:19 -0300

    feat: add height step component

---
+++ foodiary-mobile/src/components/SignUpSteps.tsx/HeightStep.tsx
@@ -0,0 +1,26 @@
+import { Controller, useFormContext } from 'react-hook-form';
+import { Input } from '../Input';
+import { signUpSchemaType } from '../../schemas/signupSchema';
+
+
+export function HeightStep() {
+  const { control } = useFormContext<signUpSchemaType>();
+
+  return (
+    <Controller
+      control={control}
+      name="height"
+      render={({ field, fieldState }) => (
+        <Input
+          label="Altura"
+          placeholder="Ex: 175"
+          value={field.value}
+          onChangeText={field.onChange}
+          error={fieldState.error?.message}
+          keyboardType="numeric"
+          append="cm"
+        />
+      )}
+    />
+  );
+}
