a3e60269576fdfdea5f1964ef4aa78707b9ee021
Guilherme Fonseca <guilhermao1234@hotmail.com>
2025-08-10 05:02:00 -0300

    feat: add BirthDate component

---
+++ foodiary-mobile/src/components/SignUpSteps.tsx/BirthDate.tsx
@@ -0,0 +1,25 @@
+import { Controller, useFormContext } from "react-hook-form";
+import { Input } from "../Input";
+import { signUpSchemaType } from "../../schemas/signupSchema";
+
+export function BirthDateStep() {
+  const { control } = useFormContext<signUpSchemaType>();
+
+  return (
+    <Controller
+      control={control}
+      name="birthDate"
+      render={({ field, fieldState }) => (
+        <Input
+          label="Data de nascimento"
+          mask="99/99/9999"
+          placeholder="DD/MM/AAAA"
+          value={field.value}
+          onChangeText={field.onChange}
+          error={fieldState.error?.message}
+          keyboardType="numeric"
+        />
+      )}
+    />
+  );
+}
