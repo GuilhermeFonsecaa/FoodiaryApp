4db174a21b637c52681ff4d79807f5f88ebe931d
Guilherme Fonseca <guilhermao1234@hotmail.com>
2025-08-13 18:16:14 -0300

    feat: listing created meals

--- foodiary-mobile/src/components/MealsList.tsx
+++ foodiary-mobile/src/components/MealsList.tsx
@@ -1,46 +1,50 @@
-import { FlatList, View } from "react-native";
+import { FlatList, View, Text } from "react-native";
 import { MealCard } from "./MealCard";
 import { MealsListHeader } from "./MealsListHeader";
 import { Separator } from "./Separator";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useQuery } from "@tanstack/react-query";
+import { httpClient } from "../services/httpClient";
 
-const meals = [
-  {
-    id: String(Math.random()),
-    name: "Café da manhã",
-  },
-  {
-    id: String(Math.random()),
-    name: "Almoço",
-  },
-  {
-    id: String(Math.random()),
-    name: "Janta",
-  },
-
-  {
-    id: String(Math.random()),
-    name: "Café da manhã",
-  },
-  {
-    id: String(Math.random()),
-    name: "Almoço",
-  },
-  {
-    id: String(Math.random()),
-    name: "Janta",
-  },
-];
+type Meals = {
+  name: string;
+  id: string;
+  icon: string;
+  foods: {
+    name: string;
+    quantity: string;
+    calories: number;
+    proteins: number;
+    carbohydraters: number;
+    fasts: number;
+  }[];
+  createdAt: Date;
+};
 
 export function MealsList() {
   const { bottom } = useSafeAreaInsets(); //retorna as margens seguras da tela
+
+  const { data: meals } = useQuery({
+    queryKey: ["meals"],
+    queryFn: async () => {
+      const { data } = await httpClient.get<{ meals: Meals[] }>("/meals", {
+        params: {
+          date: "2025-08-13",
+        },
+      });
+
+      return data.meals;
+    },
+  });
+
   return (
     <FlatList
       data={meals}
       keyExtractor={(meal) => meal.id}
-      contentContainerStyle={{ paddingBottom: 80 + bottom + 16}} //somado para garantir que o conteúdo da lista não fique colado ou escondido na parte inferior da tela
+      contentContainerStyle={{ paddingBottom: 80 + bottom + 16 }} //somado para garantir que o conteúdo da lista não fique colado ou escondido na parte inferior da tela
       ListHeaderComponent={MealsListHeader} //componente React que será renderizado no topo da lista, antes do primeiro item
       ItemSeparatorComponent={Separator} //componente React que será renderizado entre os itens da lista, funcionando como um separador visual.
+      ListEmptyComponent={<Text className="mx-5">Nenhuma refeição cadastrada...</Text>}
       renderItem={({ item: meal }) => (
         <View className="mx-5">
           <MealCard id={meal.id} name={meal.name} />
--- foodiary-mobile/src/contexts/AuthContext.tsx
+++ foodiary-mobile/src/contexts/AuthContext.tsx
@@ -105,7 +105,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   return (
     <AuthContext.Provider
       value={{
-        isLoggedIn: !!token,
+        isLoggedIn: !!user,
         isLoading: isLoadingToken || isFetching,
         signIn,
         signUp,
