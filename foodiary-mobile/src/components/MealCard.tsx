2f929bfe9bcbaf6613127623fafab363e0413d8b
Guilherme Fonseca <guilhermao1234@hotmail.com>
2025-08-19 04:09:32 -0300

    feat: add meal data on meal card

--- foodiary-mobile/src/components/MealCard.tsx
+++ foodiary-mobile/src/components/MealCard.tsx
@@ -1,30 +1,40 @@
-import { Link } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Link } from 'expo-router';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { formatMealDate } from '../utils/formatMealDate';
 
-interface MealCardProps {
+interface IMealCardProps {
   id: string;
+  createdAt: Date;
+  icon: string;
   name: string;
+  foods: { name: string }[];
 }
 
-export function MealCard({ id, name }: MealCardProps) {
+export function MealCard({
+  createdAt,
+  foods,
+  icon,
+  id,
+  name,
+}: IMealCardProps) {
   return (
     <Link href={`/meals/${id}`} asChild>
       <TouchableOpacity>
-        <Text className="text-gray-700 font-sans-regular text-base">
-          Hoje, 12h25
+        <Text className="text-base font-sans-regular text-gray-700">
+          {formatMealDate(createdAt)}
         </Text>
 
-        <View className="flex-row px-4 py-5 mt-2 gap-3 border border-gray-400 bg-white rounded-2xl">
+        <View className="mt-2 px-4 py-5 flex-row gap-3 items-center border border-gray-400 rounded-2xl">
           <View className="size-12 bg-gray-200 rounded-full items-center justify-center">
-            <Text>a</Text>
+            <Text>{icon}</Text>
           </View>
 
           <View>
-            <Text className="text-base text-gray-700 font-sans-regular">
+            <Text className="text-base font-sans-regular text-gray-700">
               {name}
             </Text>
-            <Text className="font-sans-medium text-black-700 text-base">
-              Pão, manteiga e café
+            <Text className="text-base font-sans-medium text-black-700">
+              {foods.map(({ name }) => name).join(', ')}
             </Text>
           </View>
         </View>
