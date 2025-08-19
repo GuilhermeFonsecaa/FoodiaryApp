70503f0fd3712b179bc209d06751f88aab1971b7
Guilherme Fonseca <guilhermao1234@hotmail.com>
2025-08-19 04:00:16 -0300

    fix: process meal update

--- foodiary-api/src/controllers/ProcessMeal.ts
+++ foodiary-api/src/controllers/ProcessMeal.ts
@@ -64,7 +64,7 @@ export class ProcessMeal {
               calories: 100,
               proteins: 200,
               carbohydraters: 300,
-              fasts: 400,
+              fats: 400,
             },
           ],
         })
